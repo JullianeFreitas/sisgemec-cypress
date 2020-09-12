69b7347e6b3a2f7e7f3ff0f42606d04116ebb808
Julliane Freitas <jullianefreitas@gmail.com>
2020-09-11 23:50:13 -0300

    Validação após logar no sistema

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,6 @@ import 'cypress-file-upload';
  Cypress.Commands.add("login", (usuario, password) => {
     cy.get('#login').type(usuario).tab().type(password)
     cy.get('.jumbotron > .center').click()
+    expect(cy.get('.btn-small > .material-icons'), {timeout: 25000}).to.exist
  })
  
